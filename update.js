861d9b43728ba0d03ee16eb3e7ae6fe79d7d9766
Andrey Belym <belym.a.2105@gmail.com>
2017-06-13 12:22:59 +0300

    Enable devices.md autogeneration

--- update.js
+++ update.js
@@ -3,8 +3,9 @@ var path    = require('path');
 var request = require('request-promise');
 
 
-var JSON_URL  = 'https://chromium.googlesource.com/chromium/blink/+/master/Source/devtools/front_end/emulated_devices/module.json?format=TEXT';
-var FILE_PATH = path.join(__dirname, 'index.json');
+var JSON_URL    = 'https://chromium.googlesource.com/chromium/blink/+/master/Source/devtools/front_end/emulated_devices/module.json?format=TEXT';
+var FILE_PATH   = path.join(__dirname, 'index.json');
+var TITLES_PATH = path.join(__dirname, 'devices.md');
 
 request({ url: JSON_URL })
     .then(function (body) {
@@ -16,4 +17,8 @@ request({ url: JSON_URL })
         });
         
         fs.writeFileSync(FILE_PATH, JSON.stringify(db, null, 2));
+        
+        var titles = db.map(x => '  * ' + x.title).join('\n\n');
+        
+        fs.writeFileSync(TITLES_PATH, '## Titles of Emulated Devices\n\n' + titles);
     });
