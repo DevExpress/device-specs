9576c209c18a0479990b24d88b449f3ce41bec7d
AlexKamaev <alexikam@gmail.com>
2020-01-23 13:05:08 +0300

    Add utility functions (#3)

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const emulatedDevices = require('./emulated-devices');
 const viewportSizes   = require('./viewport-sizes');
 
-module.exports = { emulatedDevices, viewportSizes };
+const {
+    getDevicesViewportData,
+    getViewportSize,
+    isValidDeviceName
+} = require('./utils');
+
+module.exports = {
+    emulatedDevices,
+    viewportSizes,
+    getDevicesViewportData,
+    getViewportSize,
+    isValidDeviceName
+};
